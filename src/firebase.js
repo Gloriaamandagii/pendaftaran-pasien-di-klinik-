d79cbec5c4f6e1733cc971457d703d1a6c415c53
virgiliotambun <s22210496@student.unklab.ac.id>
2025-11-27 12:37:37 +0800

    finalfirebash + merapikan sruktur

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = { /* ...your config... */ };
-
-const app = initializeApp(firebaseConfig);
-const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
-
-export { app, analytics };
